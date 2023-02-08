c5c18d0b3a2e5418b45558a54243e9f1f3b140a0
Joel <developerssoftlab@ipt.pe>
2023-02-07 23:47:00 -0500

    Get search params in Search page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import { useSearchParams } from "react-router-dom";
 
 type Props = {}
 
 const Search = (props: Props) => {
+    const [searchParams] = useSearchParams();
+    let search = searchParams.get('q')
     return (
-        <div>Search</div>
+        <div>{search}</div>
     )
 }
 
